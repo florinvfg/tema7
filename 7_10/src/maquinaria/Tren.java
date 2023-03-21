11082d16035cffeebf9d53cfce16c5144b88d93f
Florin Giany Virlan <126181506+florinvfg@users.noreply.github.com>
2023-03-21 20:49:09 +0100

    Add files via upload

---
+++ 7_10/src/maquinaria/Tren.java
@@ -0,0 +1,30 @@
+package maquinaria;
+import personal.Maquinista;
+public class Tren {
+    
+    Locomotora locomotora;
+    Vagon vagones[];
+    Maquinista responsable;
+    private int numVagones;
+    public Tren (Locomotora locomotora, Maquinista responsable){
+        this.locomotora = locomotora;
+        this.responsable = responsable;
+        numVagones = 0;
+        vagones = new Vagon[5];
+    }
+public void enganchaVagon(int cargaMax, int cargaActual, String mercancia) {
+    if (numVagones >= 5){
+        System.out.println("La maquinaria ya está llena");
+        
+    }else{
+        Vagon v = new Vagon(numVagones, cargaMax, cargaActual, mercancia);
+        vagones[numVagones] = v;
+        numVagones++;
+    }
+}
+}
+
+
+
+
+
---
+++ 7_10/src/personal/Maquinista.java
@@ -0,0 +1,33 @@
+/*7.10
+Hemos recibido en encargo de un cliente para definir los paquetes y las clases necesaria
+(solo implementar los atributos y los constructores) para gestionar una empresa ferroviaria,
+en el que se distinguen dos grandes grupos:el personal y la maquinaria.
+En el primero se ubican todos los empleados de la empresa,que se clasifican en tres grupos:
+los maquinistas, los mecanicos y los jefes de estacion.De cada uno de ellos es necesario guardar:
+-Maquinistas:su nombre,DNI,sueldo y el rango que tiene adquirido.
+-Mecanicos:su nombre,telefono(para contactar en caso de urgencia)y en que especialidad desarrollan
+su trabajo(esta puede ser:frenos,hidraulica,electricidad o motor)
+-Jefes de estacion:su nombre,DNI y la fecha en la que fue nombrado jefe de estacion.
+En la parte de maquinaria podemos encontrar trenes,locomotoras y vagones.De cada uno de ellos hay que considerar:
+-Vagones:tienen un numero que los identificanuna carga maxima(en kilos),la carga actual y el tipo de mercancia
+con el que estan cargados.
+-Locomotoras: disponen de una matricula(que le identifica),la potencia de sus motores y una antiguedad 
+(año de fabricacion).Ademas,cada locomotora tiene asignado un mecanico que se encargade su mantenimiento.
+-Trenes:estan formados por una locomotora y un maximo de 5 vagones.Cada tren tiene asignado un maquinista
+que es responsable de el.
+
+*/
+
+
+import java.util.*;
+public class Maquinista {
+    public static void main(String[] args) throws Exception {
+       
+
+
+
+
+
+
+    }
+}
